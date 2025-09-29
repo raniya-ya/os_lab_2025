508e860affcc9dc0ec5d353d256c4dc5febcbfad
raniya-ya <raniamuhsinova@gmail.com>
2025-09-29 23:50:41 +0000

    reversing words!

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,18 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	char *start = str;
+	char *end = str + strlen(str) - 1;
+	
+	while (start < end) {
+		char x = *start;
+		*start = *end;
+		*end = x;
+
+		start++;
+		end--;
+	}
 }
 
